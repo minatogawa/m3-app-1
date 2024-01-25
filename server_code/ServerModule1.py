7be14053f9127ea50ed91ef7aa60cabad99e989b
minatogawavlf@gmail.com <minatogawavlf@gmail.com>
2024-01-25 18:27:36 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -14,4 +14,22 @@ def processar_bibtex(blob_media):
     bibtex_database = bibtexparser.parse_string(bibtex_str)
     # Processamento adicional conforme necessário...
     # Por exemplo, extrair informações específicas do BibTeX
-    return str(bibtex_database.entries)  # Retorna uma representação string das entradas
+    # Debug: Imprime as entradas brutos e seus fields_dict
+    for entrada in bibtex_database.entries:
+        # print(entrada)  # Imprime a entrada bruta
+        print(entrada.fields_dict)  # Imprime o dicionário de campos
+  
+    # Formata e organiza as entradas BibTeX
+    entradas_formatadas = formatar_entradas(bibtex_database.entries)
+    return entradas_formatadas
+    # return str(bibtex_database.entries)  # Retorna uma representação string das entradas
+
+def formatar_entradas(entradas):
+    entradas_organizadas = []
+    for entrada in entradas:
+        # Utiliza fields_dict para acessar os campos da entrada
+        titulo = entrada.fields_dict.get('title', 'Sem Título')
+        autor = entrada.fields_dict.get('author', 'Autor Desconhecido')
+        entrada_formatada = f'Título: {titulo}, Autor(es): {autor}'
+        entradas_organizadas.append(entrada_formatada)
+    return entradas_organizadas
