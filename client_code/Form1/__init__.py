9a86ade01b86543c7a6c6d5919600743360a32e5
minatogawavlf@gmail.com <minatogawavlf@gmail.com>
2024-01-26 18:37:13 +0000

    nomes dos autores no datagrid do frontend

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,23 +2,12 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 
-
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # Configuração inicial do DataGrid
-    self.configure_data_grid()
-
     # Any code you write here will run before the form opens.
 
-  def configure_data_grid(self):
-    # Adiciona uma coluna ao DataGrid
-    self.data_grid_1.columns = [
-       {'title': "Autores", 'data_key': "autores"}
-    ]
-
-  
   def button_1_click(self, **event_args):
     resposta = anvil.server.call('testar_conexao')
     print(resposta)
@@ -37,18 +26,27 @@ class Form1(Form1Template):
                               name=self.loaded_file.name)
       
       # Chama a função no servidor e passa o BlobMedia
-      processed_data = anvil.server.call('processar_bibtex', blob)
+      lista_autores = anvil.server.call('processar_bibtex', blob)
       
-      # Exibe os dados processados
-      self.label_resultado.text = processed_data
+      # Cria um Data Grid
+      grid = DataGrid()
+      grid.columns = [
+          {"id": "author_name", "title": "Nome do Autor", "data_key": "name"}
+      ]
 
-      if processed_data:
-        self.data_grid_1.items = [{'autores': autores} for autores in processed_data]
-      else:
-        print("Erro ao obter autores")
-      
+      # Cria um RepeatingPanel
+      rp = RepeatingPanel(item_template=DataRowPanel)
+      rp.items = [{"name": autor} for autor in lista_autores]
+
+      # Adiciona o RepeatingPanel ao Data Grid
+      grid.add_component(rp)
+
+      # Adiciona o Data Grid ao formulário
+      self.add_component(grid)
     else:
       print("Nenhum arquivo carregado")
+      
+
     
 
 
