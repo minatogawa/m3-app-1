11fb9c36da7875b397386ae716f7803489707b87
minatogawavlf@gmail.com <minatogawavlf@gmail.com>
2024-01-27 19:30:21 +0000

    Edited theme.css and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,7 +7,7 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-
+    
   def button_1_click(self, **event_args):
     resposta = anvil.server.call('testar_conexao')
     print(resposta)
@@ -18,7 +18,6 @@ class Form1(Form1Template):
 
   # No evento de clique do botão (para uso futuro)
   def my_button_click(self, **event_args):
-    print("olá")
     if self.loaded_file:
       # Cria um objeto BlobMedia a partir do arquivo carregado
       blob = anvil.BlobMedia(content_type=self.loaded_file.content_type,
@@ -26,24 +25,26 @@ class Form1(Form1Template):
                               name=self.loaded_file.name)
 
       dados = anvil.server.call('processar_bibtex_e_criar_dataframe', blob)
-
       colunas = dados["colunas"]
       linhas = dados["linhas"]
-  
+    
       # Criar o Data Grid e definir colunas
       grid = DataGrid()
-      grid.columns = [{"id": col, "title": col, "data_key": col} for col in colunas]
-  
+      grid.columns = [{"id": col, "title": col, "data_key": col, "width": 200} for col in colunas]
+      
       # Preenchendo o Data Grid
       for linha in linhas:
-          # Crie um dicionário mapeando nomes de colunas para valores de linha
-          item = {col: valor for col, valor in zip(colunas, linha)}
-          grid.add_component(DataRowPanel(item=item))
+        item = {col: valor for col, valor in zip(colunas, linha)}
+        grid.add_component(DataRowPanel(item=item))
+        
+      # Cria um FlowPanel para conter o DataGrid e aplicar a rolagem horizontal
+      flow_panel = FlowPanel()
+      flow_panel.role = 'flow-scroll-x'
+      flow_panel.add_component(grid)
+        
+      # Adicionando o FlowPanel ao painel de conteúdo do formulário
+      self.content_panel.add_component(flow_panel)
 
-      
-      # Adicionando o Data Grid ao formulário
-      self.add_component(grid)
-      
     else:
       print("Nenhum arquivo carregado")
 
