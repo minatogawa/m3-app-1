9424b95ec8fda8784fe42f0af4a490d8fbd63975
minatogawavlf@gmail.com <minatogawavlf@gmail.com>
2024-01-27 19:38:36 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -32,7 +32,7 @@ class Form1(Form1Template):
   
       # Criar o Data Grid e definir colunas
       grid = DataGrid()
-      grid.columns = [{"id": col, "title": col, "data_key": col} for col in colunas]
+      grid.columns = [{"id": col, "title": col, "data_key": col, "width": "200"} for col in colunas]
   
       # Preenchendo o Data Grid
       for linha in linhas:
@@ -46,36 +46,5 @@ class Form1(Form1Template):
       
     else:
       print("Nenhum arquivo carregado")
-
-
-
-
-
-      
-      
-    #   # Chama a função no servidor e passa o BlobMedia
-    #   lista_autores = anvil.server.call('processar_bibtex', blob)
-      
-    #   # Cria um Data Grid
-    #   grid = DataGrid()
-    #   grid.columns = [
-    #       {"id": "author_name", "title": "Nome do Autor", "data_key": "name"}
-    #   ]
-
-    #   # Cria um RepeatingPanel
-    #   rp = RepeatingPanel(item_template=DataRowPanel)
-    #   rp.items = [{"name": autor} for autor in lista_autores]
-
-    #   # Adiciona o RepeatingPanel ao Data Grid
-    #   grid.add_component(rp)
-
-    #   # Adiciona o Data Grid ao formulário
-    #   self.add_component(grid)
-    # else:
-    #   print("Nenhum arquivo carregado")
-      
-
-    
-
 
 
