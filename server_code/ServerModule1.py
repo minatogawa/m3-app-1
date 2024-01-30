5367a0a6feaf5c7a74de7e5ce65def81a98e9b7c
minatogawavlf@gmail.com <minatogawavlf@gmail.com>
2024-01-30 20:07:14 +0000

    Edited Form1, Form1.RowTemplate1, Form1.RowTemplate2, ServerModule1 and 1 more

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,10 +1,9 @@
 import anvil.server
 import bibtexparser
+from anvil.tables import app_tables
 import pandas as pd
-import io
 
-
-# Função para truncar o texto
+# Função para truncar texto
 def truncar_texto(texto, max_length=180):
     if len(texto) > max_length:
         return texto[:max_length] + '…'
@@ -12,30 +11,38 @@ def truncar_texto(texto, max_length=180):
 
 @anvil.server.callable
 def processar_bibtex_e_criar_dataframe(blob_media):
-    # Lê o arquivo como uma string
+    # Lê o arquivo como uma string e processa usando bibtexparser
     bibtex_str = blob_media.get_bytes().decode()
-    # Usa bibtexparser.parse_string para processar a string BibTeX
     bibtex_database = bibtexparser.parse_string(bibtex_str)
 
-    # Processando as entradas para garantir que todos os dados sejam serializáveis
     entradas_processadas = []
     for entrada in bibtex_database.entries:
-        # Convertendo cada entrada para um dicionário, assegurando que todos os valores sejam strings
-        entrada_dict = {chave: str(valor) for chave, valor in entrada.items()}
-        entradas_processadas.append(entrada_dict)
+        # Processamento da entrada
+        processed_entry = {
+            'author': entrada['fields_dict'].get('author', ''),
+            'title': entrada['fields_dict'].get('title', ''),
+            'journal': entrada['fields_dict'].get('journal', ''),
+            'year': int(entrada['fields_dict'].get('year', 0)) if entrada['fields_dict'].get('year') else None,
+            'volume': entrada['fields_dict'].get('volume', ''),
+            'number': entrada['fields_dict'].get('number', ''),
+            'month': entrada['fields_dict'].get('month', ''),
+            'abstract': entrada['fields_dict'].get('abstract', '')
+        }
+        app_tables.bibtex_entries.add_row(**processed_entry)
 
-    # Criando um DataFrame com os dados processados
+        # Truncar texto e adicionar na lista para o DataFrame
+        processed_entry_truncated = {k: truncar_texto(v) if isinstance(v, str) else v for k, v in processed_entry.items()}
+        entradas_processadas.append(processed_entry_truncated)
+
+    # Criação do DataFrame
     df = pd.DataFrame(entradas_processadas)
 
-    # Aplicar a função de truncar em cada célula do DataFrame
-    for coluna in df.columns:
-      df[coluna] = df[coluna].apply(lambda x: truncar_texto(x) if isinstance(x, str) else x)
-
-    # Convertendo as linhas e colunas para listas
+    # Conversão das linhas e colunas para listas
     linhas = df.values.tolist()
     colunas = df.columns.astype(str).tolist()
 
     return {"colunas": colunas, "linhas": linhas}
 
-  
-
+@anvil.server.callable
+def recuperar_dados_bibtex():
+    return app_tables.bibtex_entries.search()
